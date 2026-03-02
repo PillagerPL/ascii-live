53ed60311b2ea05a78f475eb795aa31cd3179a39
PillagerPL <osek543@gmail.com>
2026-03-02 20:46:36 +0100

    made a animation
    will update

---
+++ frames/kret.go
@@ -0,0 +1,24 @@
+package frames
+
+// This is the value stored in the FrameMap
+var Coin = DefaultFrameType(coinFrames)
+
+var coinFrames = []string{
+	`
+kret
+`, `
+Kret
+`, `
+KRet
+`, `
+KREt
+`, `
+KRET
+`, `
+kRET
+`, `
+krET
+`, `
+kreT
+`, 
+}
